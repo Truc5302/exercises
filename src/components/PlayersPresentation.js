fd06fdaa4bdacf42d7b16a8d7b0d7853d263a3d2
Truong Thuy Truc <truc5302@gmail.com>
2023-05-31 01:24:30 +0700

    Presentational and Container Components

--- src/components/PlayersPresentation.js
+++ src/components/PlayersPresentation.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Players } from './shared/ListOfPlayers'
-export default function Players() {
+export default function PlayersPresentation({ players }) {
+    //console.log(players);
     return (
         <div className='container'>
-            {Players.map((player) => (
+            {players.map((player) => (
                 <div className='column'>
                     <div className='card'>
                         <img src={player.img} />
                         <h3>{player.name}</h3>
-                        <p className='title'>{player.club}</p>
+                        <p className=' title'>{player.club}</p>
                         <p><button>Detail</button></p>
                     </div>
                 </div>
